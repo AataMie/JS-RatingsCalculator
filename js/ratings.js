c00ebf55206d2851e2ea8903cd8308098bcbce37
AataMie <57095052+AataMie@users.noreply.github.com>
2021-08-30 11:38:26 +0200

    update

--- js/ratings.js
+++ js/ratings.js
@@ -5,8 +5,8 @@ function collect_ratings() {
 
    elements.forEach(element => {
        rating = parseInt(element.id.replace("star", ""));
-       ratings.count = parseInt(element.value);
-       ratings.sum = parseInt(element.value) * rating;
+       ratings.count += parseInt(element.value);
+       ratings.sum += parseInt(element.value) * rating;
    });
 
    if (ratings.count != 0) {
@@ -16,7 +16,7 @@ function collect_ratings() {
    return ratings;
 }
 
-addEventListener('change', (document) => {
+document.addEventListener('change', () => {
     const ratings = collect_ratings()
-    document.querySelector('avarage').value = ratings.average.toFixed(2);
+    document.querySelector('#average').value = ratings.average.toFixed(2);
 })
